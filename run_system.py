4cb4bcaa9190e55e3b3fbd8ecfed4ba28e392845
kaiyanh1 <125719738+kaiyanh1@users.noreply.github.com>
2025-12-02 19:22:20 -0800

    Starter code for run_system.py

--- run_system.py
+++ run_system.py
@@ -1,1 +1,25 @@
+"""Starter code"""
+# Author: (Your Name) - Student ID: XXXXXXX
 
+from mcp_server.server import MCPServer
+from agents.architect_agent import ArchitectAgent
+from agents.coder_agent import CoderAgent
+from agents.tester_agent import TesterAgent
+from core.orchestrator import Orchestrator
+
+def run_system(requirements):
+    server = MCPServer()
+
+    architect = ArchitectAgent(model="fake-llm")
+    coder = CoderAgent(model="fake-llm", mcp_server=server)
+    tester = TesterAgent(model="fake-llm", mcp_server=server)
+
+    orchestrator = Orchestrator(architect, coder, tester)
+
+    result = orchestrator.build_app(requirements)
+
+    usage = server.call_tool("get_usage")
+    return {
+        "result": result,
+        "usage": usage
+    }
