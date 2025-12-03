608b68f1ea93ebdf4ce07bd400fdfe73d67322eb
kaiyanh1 <125719738+kaiyanh1@users.noreply.github.com>
2025-12-02 19:18:54 -0800

    Starter code architect_agent.py

--- agents/architect_agent.py
+++ agents/architect_agent.py
@@ -1,1 +1,29 @@
+"""Starter code"""
+# Author: (Your Name) - Student ID: XXXXXXX
+
+from core.usage_tracker import usage_tracker
+
+class ArchitectAgent:
+
+    def __init__(self, model):
+        self.model = model
+        self.name = "architect_agent"
+
+    def analyze_requirements(self, text):
+        """
+        Here you will use the LLM to produce:
+        - file structure
+        - module plan
+        - functions needed
+        """
+        # TODO: replace with actual LLM call
+        usage_tracker.record(self.name, 150)
+
+        return {
+            "files": {
+                "main.py": "### main file template ###",
+                "culture_service.py": "### service template ###",
+                "data/cultures.json": "### data placeholder ###"
+            }
+        }
 
