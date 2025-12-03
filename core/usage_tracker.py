a848f6eb0420015cd835a7426e34648ebed8fa62
kaiyanh1 <125719738+kaiyanh1@users.noreply.github.com>
2025-12-02 19:18:02 -0800

    Starter code for usage_tracker.py

--- core/usage_tracker.py
+++ core/usage_tracker.py
@@ -1,1 +1,18 @@
+"""Starter code"""
+# Author: (Your Name) - Student ID: XXXXXXX
 
+class UsageTracker:
+    def __init__(self):
+        self.data = {}
+
+    def record(self, agent_name, tokens):
+        if agent_name not in self.data:
+            self.data[agent_name] = {"numApiCalls": 0, "totalTokens": 0}
+        self.data[agent_name]["numApiCalls"] += 1
+        self.data[agent_name]["totalTokens"] += tokens
+
+    def get_usage_report(self):
+        return self.data
+
+
+usage_tracker = UsageTracker()
